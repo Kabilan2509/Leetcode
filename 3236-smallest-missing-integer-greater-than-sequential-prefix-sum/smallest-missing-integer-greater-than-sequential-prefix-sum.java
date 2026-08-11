fac5fea1ec422184c919999b1eca0d12fe9c7ebc
Kabilan K A <kabilanka2509@gmail.com>
2026-08-11 23:38:40 +0530

    Time: 2 ms (52.86%) | Memory: 44.4 MB (23.29%) - LeetSync

--- 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -4,23 +4,26 @@ class Solution {
         int t = 0;
         int[] psum = new int[nums.length];
         HashSet<Integer> hset = new HashSet<>();
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             hset.add(nums[i]);
             t += nums[i];
             psum[i] = t;
         }
         int f = -1;
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] != nums[i - 1] + 1){
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1] + 1) {
                 sum = psum[i - 1];
                 f++;
                 break;
             }
         }
-        if(f == -1) t = psum[nums.length - 1];
-        else t = sum;
-        while(true){
-            if(!hset.contains(t)) return t;
+        if (f == -1)
+            t = psum[nums.length - 1];
+        else
+            t = sum;
+        while (true) {
+            if (!hset.contains(t))
+                return t;
             t++;
         }
     }
