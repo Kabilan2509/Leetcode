dbf9d6939105c3da0e1f3b4df3cfd3368cfe6f06
Kabilan K A <kabilanka2509@gmail.com>
2026-08-13 21:50:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (88.04%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,22 +1,21 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         String[] words = s.split(" ");
-        // System.out.println(Arrays.toString(words));
-        HashMap<Character, String> hmap = new HashMap<>();
+        String[] freq = new String[26];
+        HashSet<String> seen = new HashSet<>();
         if (pattern.length() != words.length) {
-            // System.out.println(Arrays.toString(words));
             return false;
         }
         for (int i = 0; i < words.length; i++) {
-            char c = pattern.charAt(i);
-            if (hmap.containsKey(c)) {
-                if (!hmap.get(c).equals(words[i])) {
+            int c = pattern.charAt(i) - 'a';
+            if (freq[c] != null) {
+                if (!freq[c].equals(words[i])) {
                     return false;
                 }
-            } 
-            else {
-                if(hmap.containsValue(words[i])) return false;
-                hmap.put(c, words[i]);
+            } else {
+                if (!seen.add(words[i]))
+                    return false;
+                freq[c] = words[i];
             }
         }
         return true;
