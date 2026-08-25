5f253f34360f4b99974bf9160f0824bb6a8cb0da
Kabilan K A <kabilanka2509@gmail.com>
2026-08-25 23:36:01 +0530

    Time: 1 ms (82.00%) | Memory: 46.5 MB (6.83%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -2,12 +2,12 @@ class Solution {
     public int missingMultiple(int[] nums, int k) {
         int[] freq = new int[1001];
         int maxx = Integer.MIN_VALUE;
-        for(int i : nums){
+        for (int i : nums) {
             freq[i]++;
-            maxx = Math.max(maxx,i);
+            maxx = Math.max(maxx, i);
         }
-        for(int i = 1; i <= nums.length; i++){
-            if(freq[i * k] == 0){
+        for (int i = 1; i <= nums.length; i++) {
+            if (freq[i * k] == 0) {
                 return i * k;
             }
         }
