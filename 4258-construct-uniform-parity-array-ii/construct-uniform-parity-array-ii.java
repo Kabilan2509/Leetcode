fad37f84707a6575f5fa0c1b847c50cd78eaf716
Kabilan K A <kabilanka2509@gmail.com>
2026-09-03 09:30:02 +0530

    Time: 9 ms (34.78%) | Memory: 121.5 MB (74.64%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int odd_c = 0;
         int minn_o = Integer.MAX_VALUE;
         for (int i : nums1) {
             if (i % 2 != 0) {
-                odd_c++;
                 minn_o = Math.min(minn_o, i);
             }
         }
         for (int i : nums1) {
-            if (i % 2 == 0 && i < minn_o && odd_c != 0) {
+            if (i % 2 == 0 && i < minn_o && minn_o != Integer.MAX_VALUE) {
                 return false;
             }
         }
