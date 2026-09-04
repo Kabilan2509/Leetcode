db880106ad1ffb760279594b904b89af7b99570d
Kabilan K A <kabilanka2509@gmail.com>
2026-09-04 22:08:04 +0530

    Time: 1 ms (99.79%) | Memory: 46.4 MB (49.36%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -3,16 +3,8 @@ class Solution {
         int n = nums.length;
         int[] minn = new int[n];
         Arrays.fill(minn,Integer.MAX_VALUE);
-        int[] maxx = new int[n];
-        Arrays.fill(maxx,Integer.MIN_VALUE);
         int ma = Integer.MIN_VALUE;
         int mi = Integer.MAX_VALUE;
-        for(int i = 0; i < n; i++){
-            if(ma < nums[i]){
-                ma = nums[i];
-            }
-            maxx[i] = ma;
-        }
         for(int i = n - 1; i >= 0; i--){
             if(mi > nums[i]){
                 mi = nums[i];
@@ -20,7 +12,8 @@ class Solution {
             minn[i] = mi;
         }
         for(int i = 0; i < n; i++){
-            if(maxx[i] - minn[i] <= k){
+            ma = Math.max(ma,nums[i]);
+            if(ma - minn[i] <= k){
                 return i;
             }
         }
