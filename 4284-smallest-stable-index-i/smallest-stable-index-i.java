5f87f7484074044411319d60fffcf6060f3e330e
Kabilan K A <kabilanka2509@gmail.com>
2026-09-04 22:12:42 +0530

    Time: 1 ms (99.79%) | Memory: 46.3 MB (49.36%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -3,13 +3,13 @@ class Solution {
         int n = nums.length;
         int[] minn = new int[n];
         minn[n - 1] = nums[n - 1];
-        int ma = Integer.MIN_VALUE;
-        for(int i = n - 2; i >= 0; i--){
-            minn[i] = Math.min(minn[i + 1],nums[i]);
+        int ma = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            minn[i] = Math.min(minn[i + 1], nums[i]);
         }
-        for(int i = 0; i < n; i++){
-            ma = Math.max(ma,nums[i]);
-            if(ma - minn[i] <= k){
+        for (int i = 0; i < n; i++) {
+            ma = Math.max(ma, nums[i]);
+            if (ma - minn[i] <= k) {
                 return i;
             }
         }
