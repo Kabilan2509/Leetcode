50d1d0adaf6ce853b8dae2b230f7a83a6344b4d1
Kabilan K A <kabilanka2509@gmail.com>
2026-09-04 22:10:54 +0530

    Time: 1 ms (99.79%) | Memory: 46.1 MB (88.30%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -2,7 +2,6 @@ class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
         int[] minn = new int[n];
-        // Arrays.fill(minn,Integer.MAX_VALUE);
         int ma = Integer.MIN_VALUE;
         int mi = Integer.MAX_VALUE;
         for(int i = n - 1; i >= 0; i--){
