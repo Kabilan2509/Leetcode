d4dedf4c3654d1133d1354f70d5674c4f7906040
Kabilan K A <kabilanka2509@gmail.com>
2026-09-08 16:07:36 +0530

    Time: 1 ms (83.65%) | Memory: 42.4 MB (85.26%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -3,11 +3,6 @@ class Solution {
         if(n < 1000){
             return 0;
         }
-        if(n >= 1000 && n <= 100000){
-            return n - 1000 + 1;
-        }
-        else{
-            return 4;
-        }
+        return n - 999;
     }
 }
