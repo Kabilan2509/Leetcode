f32a3711daae2e646e01ff82829a7df323529980
Kabilan K A <kabilanka2509@gmail.com>
2026-09-09 14:57:17 +0530

    Time: 1 ms (99.36%) | Memory: 42.6 MB (71.15%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -1,9 +1,9 @@
 class Solution {
     public long countCommas(long n) {
         long tot = 0;
-        for(long i = 1000; i <= n; i *= 1000){
+        for (long i = 1000; i <= n; i *= 1000) {
             tot += (n - i + 1);
-            if(i > Long.MAX_VALUE / 1000){
+            if (i > Long.MAX_VALUE / 1000) {
                 break;
             }
         }
