6a05fc8a5486aa9d968f9776935792c6173df2e1
Kabilan K A <kabilanka2509@gmail.com>
2026-09-16 21:42:34 +0530

    Time: 7 ms (67.01%) | Memory: 44.2 MB (48.07%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,27 +1,26 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()){
-            return false;
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for(char c : s1.toCharArray()){
+            freq1[c - 'a']++;
         }
-        int[] freq = new int[26];
-        for (char c : s1.toCharArray()) {
-            freq[c - 'a']++;
-        }
-        int k = s1.length();
-        int left = 0;
-        int[] window = new int[26];
-        for (int r = 0; r < s2.length(); r++) {
-            window[s2.charAt(r) - 'a']++;
-            if (r - left + 1 > k) {
-                window[s2.charAt(left) - 'a']--;
-                left++;
+        int l = 0;
+        int n = s2.length();
+        int r = 0;
+        while(r < n){
+            if(Arrays.equals(freq1,freq2)){
+                return true;
             }
-            if (r - left + 1 == k) {
-                if (Arrays.equals(window, freq)) {
-                    return true;
-                }
+            if(r - l + 1 > s1.length()){
+                freq2[s2.charAt(l) - 'a']--;
+                l++;
+            }
+            else{
+                freq2[s2.charAt(r) - 'a']++;
+                r++;
             }
         }
-        return false;
+        return Arrays.equals(freq1,freq2);
     }
 }
