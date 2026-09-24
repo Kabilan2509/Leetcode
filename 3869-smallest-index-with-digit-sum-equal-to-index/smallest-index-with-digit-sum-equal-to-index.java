8f36d7e2d2b4b7513baffa94f73782c38460fd65
Kabilan K A <kabilanka2509@gmail.com>
2026-09-24 09:25:49 +0530

    Time: 1 ms (99.69%) | Memory: 45.4 MB (77.88%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
@@ -1,15 +1,16 @@
 class Solution {
-    private int sum(int n){
+    private int sum(int n) {
         int su = 0;
-        while(n != 0){
+        while (n != 0) {
             su += n % 10;
             n /= 10;
         }
         return su;
     }
+
     public int smallestIndex(int[] nums) {
-        for(int i = 0; i < nums.length; i++){
-            if(sum(nums[i]) == i){
+        for (int i = 0; i < nums.length; i++) {
+            if (sum(nums[i]) == i) {
                 return i;
             }
         }
